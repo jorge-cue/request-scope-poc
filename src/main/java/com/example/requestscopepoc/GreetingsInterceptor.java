669e09bacadd0b983007e29993b563154aab9ce2
Jorge Horacio Cue Cantu <jorge.cue@gmail.com>
2021-08-21 20:42:02 -0500

    chore: Handler Interceptor

---
+++ src/main/java/com/example/requestscopepoc/GreetingsInterceptor.java
@@ -0,0 +1,34 @@
+package com.example.requestscopepoc;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class GreetingsInterceptor implements HandlerInterceptor {
+
+    private final RequestContext requestContext;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        log.debug("Request Context (21) {}", requestContext.getId());
+    }
+}
---
+++ src/main/java/com/example/requestscopepoc/WebConfigurer.java
@@ -0,0 +1,21 @@
+package com.example.requestscopepoc;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ *
+ */
+@Component
+@RequiredArgsConstructor
+public class WebConfigurer extends WebMvcConfigurationSupport {
+
+    private final GreetingsInterceptor greetingsInterceptor;
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(greetingsInterceptor);
+    }
+}
