85d28186918e9fb38bd43a2773283f455ba1db4f
Jorge Horacio Cue Cantu <jorge.cue@gmail.com>
2021-08-21 20:41:02 -0500

    chore: Initial commit

---
+++ src/main/java/com/example/requestscopepoc/RequestScopePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestscopepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestScopePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestScopePocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/requestscopepoc/RequestScopePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestscopepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestScopePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
